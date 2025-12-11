e343e8561ea7fbdc043ef5fc6ee59f420075ab8c
Robert <robiedick@gmail.com>
2025-12-11 15:07:34 +0100

    Commit

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--api --cors-allow-origins=http://localhost:5173 --nowebui --opt-sdp-attention
+set COMMANDLINE_ARGS=--api --cors-allow-origins=http://localhost:5173 --nowebui --opt-sdp-attention --medvram --opt-split-attention --no-half-vae
 
 call webui.bat
